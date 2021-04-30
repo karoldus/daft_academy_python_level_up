bacb0ec900cb4a06cb1a2ce9a94a3cbb5f514955
Karol Duszczyk <01158930@pw.edu.pl>
2021-04-30 16:17:30 +0200

    #3 - pd 3.5

--- main.py
+++ main.py
@@ -203,8 +203,106 @@ from fastapi import Cookie
 
 security = HTTPBasic() # do użycia BasicAuth
 
-app.last_login_session = ""
-app.last_login_token = ""
+# app.last_login_session = ""
+# app.last_login_token = ""
+
+# @app.post("/login_session", status_code=201)
+# def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)): # pobiera user i password za pomocą BasicAuth
+#     #return {"username": credentials.username, "password": credentials.password} # wydobywanie user i password
+#     correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+#     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+#     if not(correct_password and correct_username):
+#         raise HTTPException(status_code=401)
+#     session_token = hashlib.sha256(f"{credentials.username} + {credentials.password}".encode()).hexdigest()
+#     response.set_cookie(key="session_token", value=session_token)
+#     app.last_login_session = session_token
+#     return {"OK"}
+
+
+# @app.post("/login_token", status_code=201)
+# def login_token(credentials: HTTPBasicCredentials = Depends(security)):
+#     correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+#     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+#     if not(correct_password and correct_username):
+#         raise HTTPException(status_code=401)
+#     session_token = hashlib.sha256(f"{credentials.username} + {credentials.password}".encode()).hexdigest()
+#     app.last_login_token = session_token
+#     return {"token": session_token}
+
+
+
+# # 3.3
+# from fastapi.responses import PlainTextResponse
+
+# @app.get("/welcome_session")
+# def welcome_session(format:str = "", session_token: str = Cookie(None)):
+#     if session_token != app.last_login_session:
+#         raise HTTPException(status_code=401)
+#     if format == "json":
+#         return {"message": "Welcome!"}
+#     elif format == "html":
+#         return HTMLResponse(content="<h1>Welcome!</h1>", status_code=200)
+#     else:
+#         return PlainTextResponse(content="Welcome!", status_code=200)
+
+
+
+# @app.get("/welcome_token")
+# def welcome_token(token: str = "", format: str = ""):
+#     if (token == "") or (token != app.last_login_token):
+#         raise HTTPException(status_code=401)
+#     if format == "json":
+#         return {"message": "Welcome!"}
+#     elif format == "html":
+#         return HTMLResponse(content="<h1>Welcome!</h1>", status_code=200)
+#     else:
+#         return PlainTextResponse(content="Welcome!", status_code=200)
+
+
+# # 3.4 - wylogowywanie
+# from fastapi.responses import RedirectResponse
+
+# @app.delete("/logout_session")
+# def logout_session(format:str = "", session_token: str = Cookie(None)):
+#     if session_token != app.last_login_session and session_token != app.last_login_token:
+#         raise HTTPException(status_code=401)
+
+#     app.last_login_session = ""
+#     url = "/logged_out?format=" + format
+#     return RedirectResponse(url=url, status_code=303)
+
+
+
+# @app.delete("/logout_token")
+# def logout_token(token: str = "", format: str = ""):
+#     if (token == "") or (token != app.last_login_token and token != app.last_login_session):
+#         raise HTTPException(status_code=401)
+
+#     app.last_login_token = ""
+#     url = "/logged_out?format=" + format
+#     return RedirectResponse(url=url, status_code=303)
+
+
+# @app.get("/logged_out", status_code=200)
+# def logged_out(format:str = ""):
+#     if format == "json":
+#         return {"message": "Logged out!"}
+#     elif format == "html":
+#         return HTMLResponse(content="<h1>Logged out!</h1>", status_code=200)
+#     else:
+#         return PlainTextResponse(content="Logged out!", status_code=200)
+
+
+
+# 3.5
+
+# logowanie
+
+app.last_login_session = []
+app.last_login_token = []
+
+import random
+random.seed(datetime.datetime.now())
 
 @app.post("/login_session", status_code=201)
 def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)): # pobiera user i password za pomocą BasicAuth
@@ -213,9 +311,12 @@ def login_session(response: Response, credentials: HTTPBasicCredentials = Depend
     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
     if not(correct_password and correct_username):
         raise HTTPException(status_code=401)
-    session_token = hashlib.sha256(f"{credentials.username} + {credentials.password}".encode()).hexdigest()
+    secret = str(random.randint(0, 999999))
+    session_token = hashlib.sha256(f"{credentials.username}{credentials.password}{secret}".encode()).hexdigest()
     response.set_cookie(key="session_token", value=session_token)
-    app.last_login_session = session_token
+    if len(app.last_login_session) >= 3: 
+        app.last_login_session.pop(0)
+    app.last_login_session.append(session_token)
     return {"OK"}
 
 
@@ -225,18 +326,21 @@ def login_token(credentials: HTTPBasicCredentials = Depends(security)):
     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
     if not(correct_password and correct_username):
         raise HTTPException(status_code=401)
-    session_token = hashlib.sha256(f"{credentials.username} + {credentials.password}".encode()).hexdigest()
-    app.last_login_token = session_token
+    secret = str(random.randint(0, 999999))
+    session_token = hashlib.sha256(f"{credentials.username}{credentials.password}{secret}".encode()).hexdigest()
+    if len(app.last_login_token) >= 3: 
+        app.last_login_token.pop(0)
+    app.last_login_token.append(session_token)
     return {"token": session_token}
 
 
 
-# 3.3
+# dostęp
 from fastapi.responses import PlainTextResponse
 
 @app.get("/welcome_session")
 def welcome_session(format:str = "", session_token: str = Cookie(None)):
-    if session_token != app.last_login_session:
+    if session_token not in app.last_login_session:
         raise HTTPException(status_code=401)
     if format == "json":
         return {"message": "Welcome!"}
@@ -249,7 +353,7 @@ def welcome_session(format:str = "", session_token: str = Cookie(None)):
 
 @app.get("/welcome_token")
 def welcome_token(token: str = "", format: str = ""):
-    if (token == "") or (token != app.last_login_token):
+    if (token == "") or (token not in app.last_login_token):
         raise HTTPException(status_code=401)
     if format == "json":
         return {"message": "Welcome!"}
@@ -259,15 +363,15 @@ def welcome_token(token: str = "", format: str = ""):
         return PlainTextResponse(content="Welcome!", status_code=200)
 
 
-# 3.4 - wylogowywanie
+#wylogowywanie
 from fastapi.responses import RedirectResponse
 
 @app.delete("/logout_session")
 def logout_session(format:str = "", session_token: str = Cookie(None)):
-    if session_token != app.last_login_session and session_token != app.last_login_token:
+    if session_token not in app.last_login_session:
         raise HTTPException(status_code=401)
 
-    app.last_login_session = ""
+    app.last_login_session.remove(session_token)
     url = "/logged_out?format=" + format
     return RedirectResponse(url=url, status_code=303)
 
@@ -275,10 +379,10 @@ def logout_session(format:str = "", session_token: str = Cookie(None)):
 
 @app.delete("/logout_token")
 def logout_token(token: str = "", format: str = ""):
-    if (token == "") or (token != app.last_login_token and token != app.last_login_session):
+    if (token == "") or (token not in app.last_login_token):
         raise HTTPException(status_code=401)
 
-    app.last_login_token = ""
+    app.last_login_token.remove(token)
     url = "/logged_out?format=" + format
     return RedirectResponse(url=url, status_code=303)
 
@@ -291,3 +395,4 @@ def logged_out(format:str = ""):
         return HTMLResponse(content="<h1>Logged out!</h1>", status_code=200)
     else:
         return PlainTextResponse(content="Logged out!", status_code=200)
+
