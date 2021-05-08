93f539e3bde25c9dc1c57ba2caae7981e0180a4d
Karol Duszczyk <karolduszczyk@o2.pl>
2021-05-08 22:07:12 +0200

    #4 - pd 4.5 - id produktu zamiast zamówienia

--- main.py
+++ main.py
@@ -531,8 +531,8 @@ async def products_extended():
 async def products_id_orders(id: int):
     app.db_connection.row_factory = sqlite3.Row # (UnitPrice x Quantity) - (Discount x (UnitPrice x Quantity))
     data = app.db_connection.execute(f'''
-    SELECT Orders.OrderID AS id, Customers.CompanyName AS customer, [Order Details].Quantity AS quantity, [Order Details].UnitPrice AS unitprice, [Order Details].Discount as discount FROM Orders 
-    JOIN Customers ON Orders.CustomerID = Customers.CustomerID JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID WHERE Orders.OrderID = {id} ORDER BY Orders.OrderID
+    SELECT Products.ProductID AS id, Orders.OrderID, Customers.CompanyName AS customer, [Order Details].Quantity AS quantity, [Order Details].UnitPrice AS unitprice, [Order Details].Discount as discount 
+    FROM Products JOIN [Order Details] ON Products.ProductID = [Order Details].ProductID JOIN Orders ON [Order Details].OrderID = Orders.OrderID JOIN Customers ON Orders.CustomerID = Customers.CustomerID WHERE Products.ProductID = {id} ORDER BY Orders.OrderID
     ''').fetchall()
     if data == None:
         raise HTTPException(status_code=404)
