a356a3b1a4f15c5aacadbc46f9798e13996f1c54
karoldus <43970304+karoldus@users.noreply.github.com>
2021-05-15 15:45:58 +0200

    5.1

--- app/crud.py
+++ app/crud.py
@@ -11,3 +11,13 @@ def get_shipper(db: Session, shipper_id: int):
     return (
         db.query(models.Shipper).filter(models.Shipper.ShipperID == shipper_id).first()
     )
+
+# 5.1
+def get_suppliers(db: Session):
+    return db.query(models.Supplier.SupplierID, models.Supplier.CompanyName).order_by(models.Supplier.SupplierID).all()
+
+
+def get_supplier(db: Session, s_id: int):
+    return (
+        db.query(models.Supplier).filter(models.Supplier.SupplierID == s_id).first()
+    )
