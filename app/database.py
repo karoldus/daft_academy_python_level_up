91f3a063711b39551d2e6c19415d091f61b5055d
karoldus <43970304+karoldus@users.noreply.github.com>
2021-05-15 15:31:48 +0200

    ręczny url

--- app/database.py
+++ app/database.py
@@ -2,9 +2,8 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import psycopg2
 
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = "postgresql://jevmwadvvvfcze:50a5227667827a3211b3338f6ba02d429234a380cbf9b37431fbbe9db1a7a68b@ec2-54-228-9-90.eu-west-1.compute.amazonaws.com:5432/d8gk62d1rk0ujs"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
