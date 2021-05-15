9b8741f6cd94653437d27fc4b06b2d441b6aad4a
karoldus <43970304+karoldus@users.noreply.github.com>
2021-05-15 15:23:39 +0200

    zmiana sposobu łączenia z bazą

--- app/database.py
+++ app/database.py
@@ -2,8 +2,9 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
